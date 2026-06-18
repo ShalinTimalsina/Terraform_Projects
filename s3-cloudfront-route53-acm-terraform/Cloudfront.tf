be7f5ab4b5de0d891d54ff83105f0b0b91b8c627
ShalinTimalsina <shalintimalsina12@gmail.com>
2026-06-18 14:26:07 +0545

    Added a test file in the test branch

--- s3-cloudfront-route53-acm-terraform/Cloudfront.tf
+++ s3-cloudfront-route53-acm-terraform/Cloudfront.tf
@@ -1,7 +1,7 @@
 resource "aws_cloudfront_origin_access_control" "oac" {
   name                              = "project-oac"
   description                       = "Example Policy"
-  origin_access_control_origin_type = "s3"
+  origin_access_control_origin_type = "s3" 
   signing_behavior                  = "always"
   signing_protocol                  = "sigv4"
   
