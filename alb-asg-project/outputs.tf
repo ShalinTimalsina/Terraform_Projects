0b76f48fba3c60917dce8f6d60dfe1d8315c719f
ShalinTimalsina <shalintimalsina12@gmail.com>
2026-06-19 08:50:43 +0545

    Changed the live alb dns description

--- alb-asg-project/outputs.tf
+++ alb-asg-project/outputs.tf
@@ -1,5 +1,5 @@
 output "live_alb_dns" {
-  description = "This is the dns name of the actual alb"
+  description = "This is the dns name of the actual application load balancer"
   value       = "http://${aws_lb.my_alb.dns_name}"
 
 
